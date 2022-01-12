a4822e398ae8c4884401192289a3d4e5ed4d34a9
Artemio Urbina <aurbina@junkerhq.net>
2022-01-12 17:27:27 -0600

    - Added compile files for Genesis and Sega CD versions of the Suite

---
+++ compile_genesis.bat
@@ -0,0 +1,5 @@
+REM You need to set the variables first, as in the sgdk.bat file form the
+REM repository at https://github.com/ArtemioUrbina/SGDK_1_11_Suite
+
+%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen %1%
+
---
+++ compile_segaCD.bat
@@ -0,0 +1,6 @@
+REM You need to set the variables first, as in the sgdk.bat file form the
+REM repository at https://github.com/ArtemioUrbina/SGDK_1_11_Suite
+
+%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.scd %1%
+
+	
