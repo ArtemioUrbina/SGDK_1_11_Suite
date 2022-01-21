5ac7f4dadfee6b1f0e11f11cc2c8ddf68c2bcbe2
Artemio Urbina <aurbina@junkerhq.net>
2022-01-21 17:03:31 -0600

    - automated retry in make due to fork/heap errors in make

--- compile_segaCD.bat
+++ compile_segaCD.bat
@@ -1,6 +1,17 @@
 REM You need to set the variables first, as in the sgdk.bat file form the
 REM repository at https://github.com/ArtemioUrbina/SGDK_1_11_Suite
 
-%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.scd %1%
+REM since sometimes make fails due to a fork/heap error, suggested automation
+
+set count=10
+set command=%GDK_WIN%\bin\make -f %GDK_WIN%\makefile.scd %1%
+:DoWhile
+    if %count%==0 goto EndDoWhile
+    set /a count = %count% -1
+    call %command%
+    if %errorlevel%==0 goto EndDoWhile
+    if %count% gtr 0 goto DoWhile
+:EndDoWhile
+
 
 	
